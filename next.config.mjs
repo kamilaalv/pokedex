03f760c7521c0ef6b7522204547c9b37b69dc62f
kamilaalv <kamilaaliyeva58@gmail.com>
2024-06-04 02:26:58 +0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,8 @@ const nextConfig = {
                 destination: 'https://z0k5k48vq0.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
             },
             {
-                source: '/api/pokemon/:id',
-                destination: 'https://z0k5k48vq0.execute-api.eu-west-2.amazonaws.com/Prod/pokemon:id',
+                source: '/api/pokemon/id',
+                destination: 'https://z0k5k48vq0.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/id',
             },
         ];
     }
